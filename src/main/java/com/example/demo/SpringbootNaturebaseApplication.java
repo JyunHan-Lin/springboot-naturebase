5d327abfc3e28ff179e13a40f26919a69a225d52
user <user@LAPTOP-LUM34748>
2025-08-13 11:55:00 +0800

    1140813

---
+++ src/main/java/com/example/demo/SpringbootNaturebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNaturebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNaturebaseApplication.class, args);
+	}
+
+}
